11556a18480b534acc3da152f34cff85ac3ff6f9
Warren Siro <warrensiro@gmail.com>
2024-03-29 16:20:57 +0300

    created a class based view while trying to stick to conventions and created a template for such following the #<app>/object_list.html format. Also set up the url and pk relations to the template

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -12,7 +12,11 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts'
+    ordering = ['-date_posted']
 
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'Home'})
