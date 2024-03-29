189f96686d1e7436eccb39c4ea4d4c2a95e95399
Warren Siro <warrensiro@gmail.com>
2024-03-29 16:02:47 +0300

    setting up of class based views(list view), correting the template format, context naming and also rendering in urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
 
 
 def about(request):
